8c4cca77f25b8bd22c3eb8b81dfc61d093bf3961
ezzeddinp <ezzeddinpratama04@gmail.com>
2022-01-04 09:29:24 +0700

    terbaru 4 jan 2022

---
+++ htmlcourse.html/learn.js
@@ -0,0 +1,44 @@
+console.log('Hello marikho');
+console.error('This is an error!!!');
+console.warn('This is a warning!!!');
+
+// var = usualy used since the beginning & var is globally scoped
+
+
+
+// let age = 31;
+// age = 30;
+// 
+// console.log(age)
+
+
+// const name = 'marikho'; // string
+// const age = 30; // number
+// const rating = 4.5; // bool
+// const isCool = true; // 
+// let z;
+// const x = null; // null
+// const y = undefined; // undefined
+// console.log(typeof y)
+
+
+// concatenation
+// const name = 'marikho';
+// const age = 17;
+// console.log('My name is ' + name + ' and I am ' + age);
+
+// template string
+// const hello = 'My name is ${name} and I am ${age}';
+// console.log(hello)
+
+
+// const s = 'Hello Marikho'; // 13
+// console.log(s.length);
+
+// const s = 'Hello Marikho';
+// console.log(s.topUpperCase());
+
+
+const s = 'Hello Marikho!';
+console.log(s.substring(0, 5).topUpperCase());
+
